61be59aa23812efe268ca19b456e28d763f4b5c7
Maryam Ashfaq <01-135201-037@student.bahria.edu.pk>
2024-11-13 04:41:22 +0500

    NEW

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from "path"
-
 export default defineConfig({
+  base: '/Skateboarding/', // Add this line
   plugins: [react()],
   resolve: {
     alias: {
